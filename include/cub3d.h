5121d74a3667e71da5b31b5d0b901be5cb838f89
Adri Rommers <arommers@f0r1s12.codam.nl>
2023-10-23 12:55:23 +0200

    Committing changes in MLX42

--- include/cub3d.h
+++ include/cub3d.h
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   main.c                                             :+:    :+:            */
+/*   cub3d.h                                            :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: arommers <arommers@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2023/10/23 11:42:30 by arommers      #+#    #+#                 */
-/*   Updated: 2023/10/23 11:46:10 by arommers      ########   odam.nl         */
+/*   Created: 2023/10/23 11:39:00 by arommers      #+#    #+#                 */
+/*   Updated: 2023/10/23 11:40:45 by arommers      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./include/cub3d.h"
+#ifndef CUB3D_H
+# define CUB3D_H
 
-int main()
-{
+# include "../MLX42/include/MLX42/MLX42.h"
+# include <stdlib.h>
+# include <stdio.h>
 
-    return 0;
-}
-
+#endif
