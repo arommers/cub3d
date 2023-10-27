11ac894b61c2320972bb12fa1aa09f362f6e76b0
Adri Rommers <arommers@f0r1s12.codam.nl>
2023-10-27 11:55:40 +0200

    added tentative player and line struct

--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 /*   By: arommers <arommers@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/23 11:42:30 by arommers      #+#    #+#                 */
-/*   Updated: 2023/10/25 16:11:34 by arommers      ########   odam.nl         */
+/*   Updated: 2023/10/27 11:26:29 by arommers      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,97 +67,6 @@ void draw_map(mlx_t *mlx)
         }
     }
 }
-
-// void draw_rays(void *param)
-// {
-// 	mlx_t	*mlx = param;
-// 	int32_t color = ft_pixel(255, 255, 0, 255);
-	
-// 	int r, mx, my, mp, dof;
-// 	float rx, ry, ra, xo, yo;
-	
-// 	ra = pa;
-// 	for (r = 0; r < 1; r++)
-// 	{
-// 		// Check horizontal lines
-// 		dof = 0;
-// 		xo = 0;
-// 		yo = 0;
-// 		float aTan = -1/tan(ra);
-// 		if (ra > PI)
-// 		{
-// 			ry =((player->instances[0].y >> 6) << 6) - 0.0001;
-// 			rx = ((player->instances[0].y - ry) * aTan + player->instances[0].x);
-// 			yo = -64;
-// 			xo = -yo * aTan;
-// 		}
-// 		if (ra < PI)
-// 		{
-// 			ry =((player->instances[0].y >> 6) << 6) + 64;
-// 			rx = ((player->instances[0].y - ry) * aTan + player->instances[0].x);
-// 			yo = 64;
-// 			xo = yo * aTan;
-// 		}
-// 		if (ra == 0 || ra == PI)
-// 		{
-// 			rx = player->instances[0].x;
-// 			ry = player->instances[0].y;
-// 			while (dof < 8)
-// 			{
-// 				mx = (int)(rx) >> 6;
-// 				my = (int) (ry) >> 6;
-// 				mp = my * mapX + mx;
-// 				if (mp < mapX * mapY && map[mp] == 1) // hit a wall
-// 					dof = 8;
-// 				else // next line
-// 				{
-// 					rx += xo;
-// 					ry += yo;
-// 					dof += 1;
-// 				}
-// 			}
-// 		}
-// 				// Check vertical lines
-// 		dof = 0;
-// 		xo = 0;
-// 		yo = 0;
-// 		float nTan = -tan(ra);
-// 		if (ra > PI2 && ra < PI3)
-// 		{
-// 			rx =((player->instances[0].x >> 6) << 6) - 0.0001;
-// 			ry = ((player->instances[0].x - rx) * nTan + player->instances[0].y);
-// 			xo = -64;
-// 			yo = -xo * nTan; // looking left
-// 		}
-// 		if (ra < PI2 || ra < PI3)
-// 		{
-// 			rx =((player->instances[0].x >> 6) << 6) + 64;
-// 			ry = ((player->instances[0].x - rx) * nTan + player->instances[0].y);
-// 			xo = 64;
-// 			yo = xo * nTan; // looking right
-// 		}
-// 		if (ra == 0 || ra == PI)
-// 		{
-// 			rx = player->instances[0].x;
-// 			ry = player->instances[0].y;
-// 			while (dof < 8)
-// 			{
-// 				mx = (int)(rx) >> 6;
-// 				my = (int)(ry) >> 6;
-// 				mp = my * mapX + mx;
-// 				if (mp < mapX * mapY && map[mp] == 1) // hit a wall
-// 					dof = 8;
-// 				else // next line
-// 				{
-// 					rx += xo;
-// 					ry += yo;
-// 					dof += 1;
-// 				}
-// 			}
-// 		}
-// 		mlx_put_pixel(wall, rx, ry, color);
-// 	}
-// }
 
 void draw_player(void* param)
 {
