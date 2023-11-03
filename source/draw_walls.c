d219339938fd074879248897198e54f4c9036dd9
Parisa Sadeghi <psadeghi@f0r1s11.codam.nl>
2023-11-03 15:55:27 +0100

    fixed the conflict

--- source/draw_walls.c
+++ source/draw_walls.c
@@ -32,32 +32,6 @@ void	draw_wall(t_data *data, int x, int start, int end)
 	int mapValue = data->map[data->ray->mapx][data->ray->mapy];
 	
 	if (mapValue == 1)
-		color = ft_pixel(255, 0, 0, 255);  
-	else if (mapValue == 2)
-		color = ft_pixel(0, 255, 0, 255); //green
-	else if (mapValue == 3)
-		color = ft_pixel(0, 0, 255, 255); //blue
-	else if (mapValue == 4)
-		color = ft_pixel(255, 108, 125, 255); //white
-	else
-		color = ft_pixel(255, 255, 0, 255); //yellow
-
-	tmp = ray->line;
-	tmp->line_height = (int)(HEIGHT / ray->perp_wall_dist);
-	tmp->draw_start = ((tmp->line_height * -1) / 2) + (HEIGHT / 2);
-	if (tmp->draw_start < 0)
-		tmp->draw_start = 0;
-	tmp->draw_end = (int)((tmp->line_height / 2) + (HEIGHT / 2));
-	if (tmp->draw_end >= HEIGHT)
-		tmp->draw_end = HEIGHT -1;
-}
-
-void	draw_wall(t_data *data, int x, int start, int end)
-{
-	int color;
-	int mapValue = data->map[data->ray->mapx][data->ray->mapy];
-	
-	if (mapValue == 1)
 		color = ft_pixel(0, 255, 255, 255);  //red
 	else if (mapValue == 2)
 		color = ft_pixel(0, 255, 0, 255); //green
