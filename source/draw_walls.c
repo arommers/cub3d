10a822ce07718aad68559a6021965ac0f8b134e9
Parisa Sadeghi <psadeghi@f0r1s11.codam.nl>
2023-11-03 15:53:19 +0100

    norm

--- source/draw_walls.c
+++ source/draw_walls.c
@@ -42,33 +42,33 @@ void	draw_wall(t_data *data, int x, int start, int end)
 	else
 		color = ft_pixel(255, 255, 0, 255); //yellow
 
-    tmp = ray->line;
-    tmp->line_height = (int)(HEIGHT / ray->perp_wall_dist);
-    tmp->draw_start = ((tmp->line_height * -1) / 2) + (HEIGHT / 2);
-    if (tmp->draw_start < 0)
-        tmp->draw_start = 0;
-    tmp->draw_end = (int)((tmp->line_height / 2) + (HEIGHT / 2));
-    if (tmp->draw_end >= HEIGHT)
-        tmp->draw_end = HEIGHT -1;
+	tmp = ray->line;
+	tmp->line_height = (int)(HEIGHT / ray->perp_wall_dist);
+	tmp->draw_start = ((tmp->line_height * -1) / 2) + (HEIGHT / 2);
+	if (tmp->draw_start < 0)
+		tmp->draw_start = 0;
+	tmp->draw_end = (int)((tmp->line_height / 2) + (HEIGHT / 2));
+	if (tmp->draw_end >= HEIGHT)
+		tmp->draw_end = HEIGHT -1;
 }
 
-void    draw_wall(t_data *data, int x, int start, int end)
+void	draw_wall(t_data *data, int x, int start, int end)
 {
-    int color;
-    int mapValue = data->map[data->ray->mapx][data->ray->mapy];
-    
-    if (mapValue == 1)
-        color = ft_pixel(0, 255, 255, 255);  //red
-    else if (mapValue == 2)
-        color = ft_pixel(0, 255, 0, 255); //green
-    else if (mapValue == 3)
-        color = ft_pixel(0, 0, 255, 255); //blue
-    else if (mapValue == 4)
-        color = ft_pixel(255, 105, 180, 255); //pink
-    else
-        color = ft_pixel(255, 255, 0, 255); //yellow
-    if (data->ray->side == 1) 
-        color = color / 2; // Reduce intensity for side walls
-    while (start < end)
-        mlx_put_pixel(data->img, x, start++, color);
+	int color;
+	int mapValue = data->map[data->ray->mapx][data->ray->mapy];
+	
+	if (mapValue == 1)
+		color = ft_pixel(0, 255, 255, 255);  //red
+	else if (mapValue == 2)
+		color = ft_pixel(0, 255, 0, 255); //green
+	else if (mapValue == 3)
+		color = ft_pixel(0, 0, 255, 255); //blue
+	else if (mapValue == 4)
+		color = ft_pixel(255, 105, 180, 255); //pink
+	else
+		color = ft_pixel(255, 255, 0, 255); //yellow
+	if (data->ray->side == 1) 
+		color = color / 2; // Reduce intensity for side walls
+	while (start < end)
+		mlx_put_pixel(data->img, x, start++, color);
 }
